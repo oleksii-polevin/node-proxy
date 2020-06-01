15610c74135179c189e7306f2c31f745bf2c5c52
Oleksii Polevin <oleksii.polevin@onix-systems.com>
2020-06-01 18:40:51 +0300

    test

--- src/service.js
+++ src/service.js
@@ -9,7 +9,7 @@ const zlib = require('zlib');
 function helloWorld(content) {
     const startIndex = content.indexOf('<body');
     const insertIndex = content.indexOf('>', startIndex);
-    const hw = '<div style="font-size: 30px; background-color: white; color: blue; position: absolute; top: 100px; left: 45%; z-index: 1000;">Hello, World!!!</div>';
+    const hw = '<div style="font-size: 30px; background-color: white; color: blue; position: absolute; top: 100px; left: 45%; z-index: 1000;">Hello, World!!!!!!!!</div>';
     return content.substr(0, insertIndex + 1) + hw + content.substr(insertIndex + 1);
 }
 
